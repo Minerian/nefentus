c2276d1d955f81dfddc5ebd343428f792065274d
cballmueller99 <christopher@ballmueller.at>
2023-03-23 00:21:50 +0100

    signup and login backend functionality to API

--- src/components/loginBox/loginBox.jsx
+++ src/components/loginBox/loginBox.jsx
@@ -3,8 +3,82 @@ import styles from "./loginBox.module.css";
 
 import Logo from "../../assets/logo/logo.png";
 import Button from "./../button/button";
+import { useState, useEffect } from "react";
 
 const LoginBox = () => {
+
+  const [Username, setUsername] = useState('');
+  const [Password, setPassword] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('token')) {
+      const paramValue = urlParams.get('token');
+      activateUser(paramValue);
+    } else {
+      console.log("Param nicht gefunden!");
+    }
+  }, []);
+
+  async function loginUser(username1, password1) {
+    const url = 'http://127.0.0.1:8080/api/auth/signin';
+  
+    const loginRequest = {
+      username: username1,
+      password: password1,
+    };
+  
+    console.log(loginRequest);
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginRequest),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Login erfolgreich:', data);
+        // Verarbeiten Sie die Antwort, z. B. Token speichern, Nutzerdaten aktualisieren, etc.
+      } else {
+        console.error('Login fehlgeschlagen:', response.statusText);
+        // Fehlerbehandlung, z. B. Fehlermeldung anzeigen
+      }
+    } catch (error) {
+      console.error('Fehler beim Login:', error);
+      // Fehlerbehandlung, z. B. Fehlermeldung anzeigen
+    }
+  }  
+
+  const activateUser = async (token) => {
+    try {
+      const response = await fetch('http://127.0.0.1:8080/api/auth/activate', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(token),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log('Account successfully activated:', data);
+      } else {
+        console.error('Error activating account:', data);
+      }
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
+
+  function handleClick() {
+    loginUser(Username,Password);
+  }
+
   return (
     <div className={styles.login}>
       <div className={styles.card}>
@@ -14,10 +88,10 @@ const LoginBox = () => {
           <h3>Welcome back</h3>
         </div>
 
-        <Input label="Email" placeholder="you@email.com" />
-        <Input label="Password" secure placeholder="Password" />
+        <Input value={Username} setState={setUsername} label="Email" placeholder="you@email.com" />
+        <Input value={Password} setState={setPassword} label="Password" secure placeholder="Password" />
 
-        <Button>Sign in</Button>
+        <Button onClick={handleClick} link="#">Sign in</Button>
 
         <div>
           <p>
