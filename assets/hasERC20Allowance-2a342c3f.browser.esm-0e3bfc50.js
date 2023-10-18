b318ad78471b3df9d2135ba4c70789b03c5daa1f
Nemanja Mijailovic <nemanja.mijailovic2912@gmail.com>
2023-10-18 18:30:59 +0200

    Updates

--- assets/hasERC20Allowance-2a342c3f.browser.esm-0e3bfc50.js
+++
@@ -1,1 +0,0 @@
-import{k as c,av as d}from"./index-987bf79b.js";async function w(a,e,n){const r=a.getProvider(),s=new c(r,e,d,{},a.storage),t=await a.getSignerAddress(),o=a.readContract.address;return(await s.readContract.allowance(t,o)).gte(n)}export{w as h};
